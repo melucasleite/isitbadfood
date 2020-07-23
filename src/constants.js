8f6f3cbe0ce5a4f7814ef3758e6b02e2a53716d1
lucas leite <me@lucasleite.us>
2020-07-22 23:45:30 -0400

    Add bootstrap and CardIsBadFood

---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const ROBBING_WATERMELON_URL =
+  "https://storage.googleapis.com/is_it_bad_food/1ef7c35c-cc89-11ea-87d0-0242ac130003.png";
+export const DEFAULT_FOOD_URL =
+  "https://storage.googleapis.com/is_it_bad_food/b6eb9fa3-906a-4e11-ad40-6d0d36faa33e.jpg";

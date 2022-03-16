38d7a73b9589a8df1034ce39fcc8882504dbcef0
Lucas Leite <me@lucasleite.us>
2022-03-15 21:18:41 -0400

    Configure the react app

--- backend/urls.py
+++ backend/urls.py
@@ -14,9 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from api.views import FoodViewSet
 from rest_framework.routers import DefaultRouter
+from django.views.generic import TemplateView
 
 router = DefaultRouter()
 router.register("foods", FoodViewSet, basename="foods")
@@ -24,4 +25,5 @@ router.register("foods", FoodViewSet, basename="foods")
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/", include(router.urls)),
+    re_path('.*',TemplateView.as_view(template_name='index.html'))
 ]
